74cfbc4c92d8811fdc0a5af6a554b0b56a5d3657
Humberto Alves <hjalves@student.dei.uc.pt>
2014-01-17 11:14:06 +0000

    Problem 35 redo

--- problem35.py
+++ problem35.py
@@ -7,24 +7,26 @@ Circular primes
 """
 
 from math import log10
+from collections import Counter
+from itertools import product, chain
 
-def digsize(n):
+def digits(n):
     return int(log10(n))+1 if n > 0 else 0
 
 def rotations(n):
-    ds = digsize(n)
-    for i in range(ds):
-        a, b = divmod(n, 10**i)
-        yield b * (10**(ds-i))+ a
+    ds = digits(n)
+    return ((n % 10**i) * (10**(ds-i)) + (n // 10**i) for i in range(ds))
 
-def is_prime(n):
+def isprime(n):
     return n >= 2 and all(n % i != 0 for i in range(2, int(n**0.5)+1))
 
-def circular_primes(max):
-    primes = set(filter(is_prime, range(max)))
-    for p in sorted(primes):
-        if all(r in primes for r in rotations(p)):
-            yield p
+def join(l):
+    return sum(b * 10**p for p, b in enumerate(reversed(l)))
 
-cp = list(circular_primes(max=1000000))
-print(cp, len(cp))
+def iter1379(dig):
+    return (join(l) for n in range(2, dig+1) for l in product(*[(1,3,7,9)]*n))
+
+iter = chain(range(2, 10), iter1379(6))
+primes = set(filter(isprime, iter))
+s = set(p for p in primes if set(rotations(p)) <= primes)
+print(len(s))
