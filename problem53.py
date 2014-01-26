bfb122fc010174ea37c51c114a5c9af723344f1f
Humberto Alves <hjalves@student.dei.uc.pt>
2014-01-26 13:37:44 +0000

    Problem 53

--- problem53.py
+++ problem53.py
@@ -5,3 +5,13 @@
 Project Euler - Problem 53
 Combinatoric selections
 """
+
+def pascal_triangle(n):
+    pos = lambda i, j: i * (i + 1) // 2 + j     # triangle indexing
+    l = [1] * pos(n+1, 0)
+    for i in range(n+1):
+        for j in range(1, i):
+            l[pos(i, j)] = l[pos(i-1, j-1)] + l[pos(i-1, j)]
+    return l
+
+print(len([n for n in pascal_triangle(100) if n > 1000000]))
