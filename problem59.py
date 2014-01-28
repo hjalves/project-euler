eef220000a081b84df485c2ccf62f01568ca085f
Humberto Alves <hjalves@student.dei.uc.pt>
2014-01-28 01:04:21 +0000

    Problem 59

--- problem59.py
+++ problem59.py
@@ -5,3 +5,19 @@
 Project Euler - Problem 59
 XOR decryption
 """
+
+from collections import Counter
+from itertools import cycle
+
+with open("problem59/cipher1.txt") as textf:
+    data = bytes(map(int, textf.read().split(',')))
+
+keya = Counter(data[0::3]).most_common(1)[0][0] ^ ord(' ')
+keyb = Counter(data[1::3]).most_common(1)[0][0] ^ ord(' ')
+keyc = Counter(data[2::3]).most_common(1)[0][0] ^ ord(' ')
+key = bytes([keya, keyb, keyc])
+
+unencrypted = bytes(x^k for x, k in zip(data, cycle(key)))
+
+print(unencrypted)
+print("sum:", sum(unencrypted))
